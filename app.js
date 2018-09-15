32b67acdff35d8f762d72894fdad5c62ae041d46
yves <yves.talboom@gmail.com>
2018-09-15 13:24:22 +0200

    Add rest of CRUD for admin pages

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const bodyparser = require('body-parser');
 const session = require('express-session');
-const flash = require('connect-flash')();
+const flash = require('connect-flash');
 const { database } = require('./config/database');
 const pages = require('./routes/pages');
 const adminPages = require('./routes/admin_pages');
@@ -27,7 +27,7 @@ app.set('trust proxy', 1); // trust first proxy
 app.use(
   session({ resave: true, saveUninitialized: true, secret: 'someSecret' }),
 );
-app.use(flash);
+app.use(flash());
 app.use(function(req, res, next) {
   res.locals.messages = require('express-messages')(req, res);
   next();
--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -99,4 +99,89 @@ router.post('/reorder-pages', (req, res) => {
   }
 });
 
+router.get('/edit-page/:slug', (req, res) => {
+  Page.findOne({ slug: req.params.slug }, (err, page) => {
+    if (err) {
+      return console.log(err);
+    }
+    const { title, slug, content, _id } = page;
+    res.render('admin/edit_page', { title, slug, content, id: _id });
+  });
+});
+
+router.post(
+  '/edit-page/:slug',
+  [
+    body('title')
+      .not()
+      .isEmpty()
+      .withMessage('title must have a value'),
+    body('content')
+      .not()
+      .isEmpty()
+      .withMessage('content must have a value'),
+  ],
+  (req, res) => {
+    const title = req.body.title;
+    let slug = req.body.slug.replace(/\s+/g, '-').toLowerCase();
+    if (slug === '') {
+      slug = title.replace(/\s+/g, '-').toLowerCase();
+    }
+
+    const { content, id } = req.body;
+
+    const errors = validationResult(req);
+    if (errors && errors.array() && errors.array().length > 0) {
+      console.log('errors:', errors.array());
+      return res.render('admin/edit_page', {
+        title,
+        slug,
+        content,
+        id,
+        errors: errors.array(),
+      });
+    } else {
+      Page.findOne({ slug, _id: { $ne: id } }, (err, page) => {
+        if (page) {
+          req.flash('danger', 'Page slug exists, choose another');
+          return res.render('admin/add_page', {
+            title,
+            slug,
+            content,
+            id,
+          });
+        } else {
+          Page.findById(id, (err, page) => {
+            if (err) {
+              return console.log(err);
+            }
+            page.title = title;
+            page.slug = slug;
+            page.content = content;
+
+            page.save(err => {
+              if (err) {
+                console.log(err);
+                return res.status(500).send({ err });
+              }
+              req.flash('success', 'page added');
+              res.redirect(`/admin/pages/edit-page/${page.slug}`);
+            });
+          });
+        }
+      });
+    }
+  },
+);
+
+router.get('/delete-page/:id', (req, res) => {
+  Page.findByIdAndRemove(req.params.id, (err, pages) => {
+    if (err) {
+      return console.log(err);
+    }
+    req.flash('success', 'page deleted');
+    res.redirect(`/admin/pages`);
+  });
+});
+
 module.exports = router;
